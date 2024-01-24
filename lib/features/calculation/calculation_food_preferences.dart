b41506872892613ba81f197b469f2e4dd26bdda2
Kabala Illya <illia.kabala@solidway.net>
2024-01-24 15:44:40 +0200

    PLAT-15 create page Food-Preferences and add fixes in routes HealthGoals page

---
+++ lib/features/calculation/calculation_food_preferences.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:platy/features/calculation/calculation_global.dart';
+import 'package:platy/features/calculation/custom_list_tile.dart';
+import 'package:platy/features/calculation/theme.dart';
+
+class CalculateFoodPreferencesWidget extends StatefulWidget {
+  const CalculateFoodPreferencesWidget({Key? key}) : super(key: key);
+
+  @override
+  _CalculateFoodPreferencesWidgetState createState() =>
+      _CalculateFoodPreferencesWidgetState();
+}
+
+class _CalculateFoodPreferencesWidgetState
+    extends State<CalculateFoodPreferencesWidget> {
+  List<String> titles = [
+    '1-2 TPD',
+    '3 TPD',
+    '4 TPD',
+    '5TPD',
+    'Only snacks',
+    'Intermediate Fasting',
+  ];
+  bool _isButtonActive = false;
+  List<bool> _isCheckedList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _isCheckedList = List.generate(titles.length, (index) => false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 73),
+        Text(
+          'Food Preferences',
+          textAlign: TextAlign.center,
+          style: whiteTheme.textTheme.bodyMedium,
+        ),
+        const Text(
+          'How often do you prefer to eat?',
+          style: TextStyle(
+            fontFamily: 'Gilroy',
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+            color: Colors.grey,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Expanded(
+          child: ListView.builder(
+            itemCount: titles.length,
+            itemBuilder: (context, index) {
+              return CustomListTile(
+                title: titles[index],
+                isChecked: _isCheckedList[index],
+                onTilePressed: (isChecked) {
+                  setState(() {
+                    _isCheckedList[index] = isChecked;
+                    _isButtonActive = _isCheckedList.contains(true);
+                  });
+                },
+              );
+            },
+          ),
+        ),
+        const SizedBox(height: 10),
+        Container(
+          height: 54.0,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25.0),
+            gradient: const LinearGradient(
+              colors: [
+                Color(0xFF59A7A7),
+                Color(0xFFAFCD6D),
+              ],
+            ),
+          ),
+          child: ElevatedButton(
+            onPressed: _isButtonActive
+                ? () {
+                    CalculateGlobalWidget.of(context).pageController.nextPage(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeIn);
+                  }
+                : null,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(22.0),
+              ),
+            ),
+            child: const Text(
+              'Next',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
